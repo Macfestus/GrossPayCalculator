17372f3139738b6cf75e8edb01ad4e5d8342301d
Macfestus <macfestus2@gmail.com>
2022-11-04 04:55:18 -0700

    added new files in intellij

---
+++ QoutaCalculator.java
@@ -0,0 +1,32 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/*
+*IF ELSE
+* All salespeople are expected to make at least 10 sales each week.
+* For those who do, they receive a congratulatory message.
+* For those who don't, they are informed of how many sales they were short.
+ */
+public class QoutaCalculator {
+
+    public static void main(String arg[]){
+        //Initialize the value known
+        int quota = 10;
+
+        //Get the value Unknown
+        System.out.println("Enter the number of sales you made this week");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        //Make a decision on the path to take - Output
+        if(sales >= quota)
+            System.out.println("congrats! you've met your quota");
+
+        else{
+            int salesShort = quota - sales;
+            System.out.println("You did not make your quota. You were " + salesShort + " sales short");
+        }
+    }
+}
